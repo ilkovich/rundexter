e5cb8178e6fcdc30e60f9b605fd8bba47452e2a6
Daniel Ilkovich <daniel+github@betaworks.com>
2015-04-09 12:48:39 -0400

    login method and user config

--- bin/dexter.bin.js
+++ bin/dexter.bin.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 var path = require('path')
+  , rest = require('restler')
+  , fs   = require('fs')
+  , prompt = require('prompt')
+  , home = process.env.HOME
   , title
 ;
 
@@ -14,17 +18,57 @@ switch(process.argv[2]) {
     case 'run': 
         run();
         break;
+    case 'login':
+        login();
+        break;
     default: 
         help();
         return;
 }
 
-function help() {
-    console.log('dexter <create|run>');
-}
+/**
+ * Log the user in and update/create global config
+ * 
+ * @access public
+ * @return void
+ */
+function login() {
+    var user    = process.argv[3]
+      , baseUrl = process.argv[4]
+      , credentials = {
+           email: user
+      }
+      , loginUrl
+    ;
 
-function helpCreate() {
-    console.log('dexter create <moduleName>');
+    if(!user) 
+        return helpLogin();
+
+    prompt.message = prompt.delimiter = '';
+    prompt.get({ properties: { password: { message: 'Password:', hidden: true }}}, function(err, result) {
+
+        if(!(credentials.password = result.password))
+            return helpLogin();
+
+        getConfig(function(config) {
+            baseUrl  = baseUrl || config.baseUrl || 'https://rundexter.com/api/';
+            loginUrl = baseUrl + 'auth/login';
+
+            rest.post(loginUrl,  {
+                data: credentials
+            }).on('complete', function(result, response) {
+                if(result && result.success) {
+                    config.token   = result.data.token;
+                    config.baseUrl = baseUrl;
+                    writeConfig(config, generateReporter());
+                } else if(result && result.error) {
+                    console.error(result.error);
+                } else {
+                    console.error(response.statusCode, result);
+                }
+            });
+        });
+    });
 }
 
 //Courtesy of http://stackoverflow.com/a/105074
@@ -47,6 +91,14 @@ function slugify(text) {
     .replace(/-+$/, '');            // Trim - from end of text
 }
 
+
+/**
+ * Create a new module
+ * 
+ * @param title $title 
+ * @access public
+ * @return void
+ */
 function create(title) {
     var name   = slugify(title)
       , mkdirp = require('mkdirp')
@@ -65,6 +117,12 @@ function create(title) {
     });
 }
 
+/**
+ * Run a fixture against the current module. 
+ * 
+ * @access public
+ * @return void
+ */
 function run() {
     var sf      = require('../StepFactory')
       , mod     = require(process.cwd())
@@ -95,4 +153,75 @@ function run() {
         });
     };
     new Runner();
+}
+
+/*
+ * HELPERS
+ */
+
+function help() {
+    console.log('dexter <create|run>');
+}
+
+function helpCreate() {
+    console.log('dexter create <moduleName>');
+}
+
+function helpLogin() {
+    console.log('dexter login <username>');
+}
+
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
+
+function getUserHome() {
+  return process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+}
+
+function getConfig(callback) {
+    var file = getConfigFilename()
+      , config
+    ;
+
+    fs.readFile(file, 'utf8', function(err, configData) {
+        if(err) config = {};
+        else {
+            try {
+                config = JSON.parse(configData);
+            } catch(e) {
+                config = {};
+            }
+        }
+
+        callback(config);
+    });
+}
+
+function getConfigFilename() {
+    return getUserHome() + '/.dexter';
+}
+
+function writeConfig(config, callback) {
+    fs.writeFile(getConfigFilename(), JSON.stringify(config, null, 4), function(err) {
+        if(err) callback(err);
+
+        callback(null, config);
+    });
+}
+
+function generateReporter(printData) {
+    return function(err, data) {
+        if(err) return console.error('ERROR', err);
+
+        if(printData) 
+            return console.log('SUCCESS', data);
+        else
+            return console.log('SUCCESS');
+    };
 }
